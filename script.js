daf630a21f0a78772e783f0834bc44731db5c8d4
Uwase Sophie <uwasesophie91@gmail.com>
2025-06-16 01:50:56 +0200

    changes made on css

--- script.js
+++ script.js
@@ -89,11 +89,21 @@ function renderTransactions() {
         <div class="transaction-date">${tx.date}</div>
       </div>
       <div class="transaction-amount ${tx.type === 'income' ? 'transaction-income' : 'transaction-expense'}">
-        ${tx.type === 'income' ? '+' : '-'} RWF${tx.amount.toFixed(2)}
+        ${tx.type === 'income' ? '+' : '-'} RWF ${tx.amount.toFixed(2)}
       </div>
       <div class="transaction-actions">
-        <button class="edit-btn" onclick="editTransaction(${index})">✏️</button>
-        <button class="delete-btn" onclick="deleteTransaction(${index})">🗑️</button>
+        <button class="edit-btn" onclick="editTransaction(${index})">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
+</svg>
+
+        </button>
+        <button class="delete-btn" onclick="deleteTransaction(${index})">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+</svg>
+
+        </button>
       </div>
     `;
 
@@ -113,7 +123,7 @@ function updateBalance() {
     .filter(tx => tx.type === 'expense')
     .reduce((sum, tx) => sum + tx.amount, 0);
 
-  balanceDisplay.textContent = `RWF${(income - expense).toFixed(2)}`;
+  balanceDisplay.textContent = `RWF ${(income - expense).toFixed(2)}`;
 }
 
 // Delete transaction
